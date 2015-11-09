c570e229722f2c7d2978f68e93ca0493e1dbb1c6
Lindsey Reno <lindseyannereno@gmail.com>
2015-11-09 15:22:33 -0800

    Registered models so they are visible on the admin site

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Customer, Movie, Rental
 
-# Register your models here.
+admin.site.register(Customer)
+admin.site.register(Movie)
+admin.site.register(Rental)
