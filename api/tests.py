5e4ed3e86d6d34247e09ec5fda0b49f150fc37c7
Lindsey Reno <lindseyannereno@gmail.com>
2015-11-13 14:03:11 -0800

    added unit tests for the Rental model, tests pass

--- api/tests.py
+++ api/tests.py
@@ -4,8 +4,8 @@ from datetime import datetime, timedelta
 from decimal import Decimal
 from django.core.validators import MinValueValidator, ValidationError
 
-# to run tests with coverage: coverage run --source='.' manage.py test api
-# then to get the coverage report: coverage report
+# to run tests with coverage: coverage run --source='.' --omit='api/migrations/*,PyVideoAPI/*,api/management/*' manage.py test api
+# then to get the coverage report: coverage report -m
 
 class CustomerTestCase(TestCase):
     def setUp(self):
@@ -20,9 +20,9 @@ class CustomerTestCase(TestCase):
     def test_valid_customer(self):
         """ Adds a customer to the database when valid params given """
         customer = Customer.objects.get(id=1)
-        self.assertEqual("Jean Luc Picard", customer.name)
-        self.assertEqual("123 Space", customer.address)
-        self.assertEqual(Decimal('56.34'), customer.account_credit)
+        self.assertEqual(customer.name, "Jean Luc Picard")
+        self.assertEqual(customer.address, "123 Space")
+        self.assertEqual(customer.account_credit, Decimal('56.34'))
 
     def test_customer_name_is_required(self):
         """ Requires name length to be greater than zero """
@@ -86,8 +86,8 @@ class MovieTestCase(TestCase):
         """ Adds a movie to the database when valid params given """
         movie = Movie.objects.get(id=1)
         self.assertEqual("Jaws", movie.title)
-        self.assertEqual(6, movie.inventory)
-        self.assertEqual(6, movie.num_available)
+        self.assertEqual(movie.inventory, 6)
+        self.assertEqual(movie.num_available, 6)
 
     def test_num_available_less_than_inventory(self):
         """ Does not allow a movie's num_available to exceed inventory. """
@@ -132,3 +132,61 @@ class MovieTestCase(TestCase):
         )
         self.assertEqual(len(movie.rental_set.all()), 1)
         self.assertEqual(movie.rental_set.first().pk, 1)
+
+class RentalTestCase(TestCase):
+    def setUp(self):
+        movie = Movie.objects.create(
+            title="Jaws",
+            overview="Sharks are super hungry",
+            release_date=datetime.now(),
+            inventory=6,
+            num_available=6
+        )
+        customer = Customer.objects.create(
+            name="Jean Luc Picard",
+            registered_at=datetime.now(),
+            address="123 Space", city="Space", state="MW",
+            postal_code="its f-ing space dude", phone="555-5555",
+            account_credit=56.34
+        )
+        rental = Rental.objects.create(
+            checkout_date=datetime.now(),
+            return_date=datetime.now() + timedelta(7,0),
+            checked_out=True,
+            movie=movie,
+            customer=customer
+        )
+    def test_valid_rental(self):
+        """ Adds a rental to the database when valid params given """
+        rental = Rental.objects.get(id=1)
+        self.assertEqual(rental.checked_out, True)
+        self.assertEqual(rental.movie.title, 'Jaws')
+        self.assertEqual(rental.customer.name, "Jean Luc Picard")
+
+    def test_rental_decrements_movie_num_available(self):
+        """ When a rental is saved it decrements the num_available of a movie """
+        movie = Movie.objects.get(id=1)
+        self.assertEqual(movie.num_available, 5)
+
+    def test_rental_fails_when_no_movies_available(self):
+        """ Raises a ValidationError if there are no more movies available to rent """
+        movie = Movie.objects.get(id=1)
+        movie.num_available = 0
+        movie.save()
+        customer = Customer.objects.create(
+            name="Jean Luc Picard",
+            registered_at=datetime.now(),
+            address="123 Space", city="Space", state="MW",
+            postal_code="its f-ing space dude", phone="555-5555",
+            account_credit=56.34
+        )
+        new_rental = Rental(
+            checkout_date=datetime.now(),
+            return_date=datetime.now() + timedelta(7,0),
+            checked_out=True,
+            movie=movie,
+            customer=customer
+        )
+        db_movie = Movie.objects.get(id=1)
+        self.assertEqual(db_movie.num_available, 0)
+        self.assertRaises(ValidationError, new_rental.save)
